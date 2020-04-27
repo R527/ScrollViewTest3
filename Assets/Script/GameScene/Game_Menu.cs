d1d381b0b7e13e4c8b07e5870fc1a8f35467becf
R <rockleeserver3@gmail.com>
2020-04-27 21:04:11 +0900

    MenuBottonの機能追加・修正中

---
+++ Assets/Script/GameScene/Game_Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// ゲームシーンにあるメニューボタンを制御する
+/// </summary>
+public class Game_Menu : MonoBehaviour
+{
+
+    public Button gameMenuButton;
+    public GameObject gameMenu;
+
+    void Start()
+    {
+        gameMenuButton.onClick.AddListener(GameMenuButton);
+    }
+
+
+    public void GameMenuButton() {
+        Instantiate(gameMenu);
+    }
+
+}
